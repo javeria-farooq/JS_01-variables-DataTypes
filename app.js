89a14907db2fa7ef5ca6620450deaefb89ce8823
javeria-farooq <gettingeducation6@gmail.com>
2024-04-23 22:12:42 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+//       // non primitive data type: objects
+const student = {
+    firstName: "Javeria",
+    lastName: "Farooq",
+    age: 20,
+    percentage: 74,
+    isPass: true
+}
+
+document.write(student.firstName + '<br>')
+
+student.firstName += " M. "
+
+document.write(student.firstName + '<br>')
+
+student.name = "Aliza"
+console.log(student)
+document.write(student.name)
+
+//            // practice questions:
+const pen = {
+    productName : "Parker Jotter standard CT ball pen (black)",
+    rating : 4,
+    isDeal : true,
+    price : 270,
+    offerPercent : 5
+}
+
+console.log(pen)
+
+const profile = {
+    userName : "@Javeria Farooq",
+    isFollow : true,
+    isMessage : true,
+    followers : 569000,
+    posts : 195,
+    following : 4,
+    institution : "Microsoft",
+    profession : "to educate someone is the highest privilege"
+}
